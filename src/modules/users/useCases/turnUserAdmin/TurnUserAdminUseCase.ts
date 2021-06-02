10d94da639e4abd6728af9c6abc597a3cf898579
wsasouza <wsasouza@hotmail.com>
2021-06-02 17:24:03 -0300

    :tada: Challenge completed

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,16 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.admin = true;
+    user.updated_at = new Date();
+
+    return user;
   }
 }
 
